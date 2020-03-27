71a068296ddc0e1559895107c804001636fd92b4
Amara Nwandu <nwandunzube@gmail.com>
2020-03-27 16:35:39 +0100

    my commit

---
+++ js.basics/index.js
@@ -0,0 +1,17 @@
+const firstName = 'Amara';
+
+console.log(firstName);
+
+const lastName = 'Nwandu';
+
+console.log(lastName);
+
+let courses = ['HTML', 'CSS', 'JavaScript'];
+
+console.table(courses);
+
+for(i = 1; i <= 200; i++){
+    if(i % 2 !== 0 || i === 200){
+        console.log(i);
+    }
+}
